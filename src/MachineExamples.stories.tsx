3111b10e34119f9acd81247e24bb4d4b3931cce3
Nick Nisi <nick@nisi.org>
2023-06-22 09:12:11 -0500

    consolidate examples

---
+++ src/MachineExamples.stories.tsx
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { RenderMachine } from 'storybook-xstate-addon/RenderMachine';
+import { createMachine } from 'xstate';
+
+const meta: Meta = {
+  title: 'Examples',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RenderMachine>;
+
+type LightEvent = { type: 'SWITCH' };
+
+const lightMachine = createMachine<undefined, LightEvent>({
+  id: 'light',
+  initial: 'red',
+  states: {
+    red: {
+      on: { SWITCH: 'green' },
+    },
+    yellow: {
+      on: { SWITCH: 'red' },
+    },
+    green: {
+      on: { SWITCH: 'yellow' },
+    },
+  },
+});
+
+export const LightMachine: Story = {
+  render: () => <RenderMachine machine={lightMachine} />,
+};
+
+const promiseMachine = createMachine({
+  id: 'Promise',
+  initial: 'pending',
+  states: {
+    pending: {
+      on: {
+        RESOLVE: 'fulfilled',
+        REJECT: 'rejected',
+      },
+    },
+    fulfilled: { type: 'final' },
+    rejected: { type: 'final' },
+  },
+});
+
+export const PromiseMachine: Story = {
+  render: () => <RenderMachine machine={promiseMachine} />,
+};
